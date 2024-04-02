e1d7610b474015c131222f95b4195750e1feaace
pach <84549814+pachxl@users.noreply.github.com>
2024-04-02 01:51:00 +0100

    tweak(resource): add tpToPlayer and bringPlayer event intercept

--- resource/menu/server/sv_player_modal.lua
+++ resource/menu/server/sv_player_modal.lua
@@ -21,24 +21,28 @@ RegisterNetEvent('txsv:req:tpToPlayer', function(tgtId)
 
   -- More OneSync dependent code
   if allow then
-    -- Check for routing bucket diff
-    local tgtBucket = GetPlayerRoutingBucket(tgtId)
-    local srcBucket = GetPlayerRoutingBucket(src)
+    if GetConvarBool("arena:txAdmin:interceptTp") then
+      TriggerEvent("arena:txAdmin:tpToPlayer", src, tgtId)
+    else
+      -- Check for routing bucket diff
+      local tgtBucket = GetPlayerRoutingBucket(tgtId)
+      local srcBucket = GetPlayerRoutingBucket(src)
 
-    -- This isn't stored anywhere for reversion,
-    -- as TP to player is typically a one sided operation
-    if tgtBucket ~= srcBucket then
-      SetPlayerRoutingBucket(src, tgtBucket)
-    end
+      -- This isn't stored anywhere for reversion,
+      -- as TP to player is typically a one sided operation
+      if tgtBucket ~= srcBucket then
+        SetPlayerRoutingBucket(src, tgtBucket)
+      end
 
-    -- ensure the player ped exists
-    local ped = GetPlayerPed(tgtId)
-    if ped then
-      local coords = GetEntityCoords(ped)
-      data.x = coords[1]
-      data.y = coords[2]
-      data.z = coords[3]
-      TriggerClientEvent('txcl:tpToCoords', src, data.x, data.y, data.z)
+      -- ensure the player ped exists
+      local ped = GetPlayerPed(tgtId)
+      if ped then
+        local coords = GetEntityCoords(ped)
+        data.x = coords[1]
+        data.y = coords[2]
+        data.z = coords[3]
+        TriggerClientEvent('txcl:tpToCoords', src, data.x, data.y, data.z)
+      end
     end
   end
 
@@ -52,11 +56,15 @@ RegisterNetEvent('txsv:req:bringPlayer', function(id)
   end
   local allow = PlayerHasTxPermission(src, 'players.teleport')
   if allow then
-    -- ensure the target player ped exists
-    local ped = GetPlayerPed(id)
-    if ped then
-      local coords = GetEntityCoords(GetPlayerPed(src))
-      TriggerClientEvent('txcl:tpToCoords', id, coords[1], coords[2], coords[3])
+    if GetConvarBool("arena:txAdmin:interceptBring") then
+      TriggerEvent("arena:txAdmin:bringPlayer", src, id)
+    else
+      -- ensure the target player ped exists
+      local ped = GetPlayerPed(id)
+      if ped then
+        local coords = GetEntityCoords(GetPlayerPed(src))
+        TriggerClientEvent('txcl:tpToCoords', id, coords[1], coords[2], coords[3])
+      end
     end
   end
   TriggerEvent('txsv:logger:menuEvent', src, 'summonPlayer', allow, id)
